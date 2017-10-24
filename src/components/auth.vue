fcce27022ea486ad07debf142f41f4bd18844c8c
qhx0807 <820240134@qq.com>
2017-10-24 23:09:09 +0800

    :construction:

---
+++ src/components/auth.vue
@@ -0,0 +1,18 @@
+<template>
+  <van-button type="default">Default</van-button>
+</template>
+
+<script>
+export default {
+    name: 'auth',
+    data () {
+        return {
+        msg: 'Welcome'
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const HelloWorld = resolve => require(['@/components/HelloWorld'], resolve)
+const Auth = resolve => require(['@/components/auth'], resolve)
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
-    }
-  ]
+	routes: [
+		{
+			path: '/',
+			name: 'Hello',
+			component: HelloWorld
+		},
+		{
+			path: '/auth',
+			name: 'auth',
+			component: Auth
+		}
+	]
 })
