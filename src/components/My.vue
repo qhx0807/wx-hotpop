92e645581cccb783b227a25ac2fa5f64539940f8
qhx0807 <820240134@qq.com>
2017-10-30 00:43:08 +0800

    update

--- src/components/My.vue
+++ src/components/My.vue
@@ -3,10 +3,19 @@
         <div class="head-img">
             <van-swipe :autoplay="3000">
                 <van-swipe-item>
-                    <img src="../assets/y1.jpg"/>
+                    <img src="../assets/s1.jpg"/>
                 </van-swipe-item>
                 <van-swipe-item>
-                    <img src="../assets/s1.jpg"/>
+                    <img src="../assets/s2.png"/>
+                </van-swipe-item>
+                <van-swipe-item>
+                    <img src="../assets/s3.jpg"/>
+                </van-swipe-item>
+                <van-swipe-item>
+                    <img src="../assets/s4.jpg"/>
+                </van-swipe-item>
+                <van-swipe-item>
+                    <img src="../assets/s5.jpg"/>
                 </van-swipe-item>
             </van-swipe>
         </div>
@@ -29,21 +38,37 @@
             </van-col>
         </van-row>
 
-        <van-cell-group>
-            <van-cell icon="records" title="全部订单" is-link>
-               
-            </van-cell>
-        </van-cell-group>
+        <div class="my-links">
+            <van-cell-group>
+                <van-cell icon="records" title="全部订单" is-link>
+                </van-cell>
+            </van-cell-group>
 
-        <van-cell-group style="margin-top:10px">
-            <van-cell title="我的购物车" icon="shopping-cart" is-link></van-cell>
-            <van-cell title="我的返现" icon="cash-back-record" is-link value="内容"></van-cell>
-        </van-cell-group>
+            <van-cell-group style="margin-top:10px">
+                <van-cell title="我的购物车" icon="shopping-cart" is-link></van-cell>
+                <van-cell title="我的返现" icon="cash-back-record" is-link value=""></van-cell>
+            </van-cell-group>
 
-        <van-cell-group style="margin-top:10px">
-            <van-cell title="我的积分" icon="exchange" is-link></van-cell>
-            <van-cell title="我的优惠券" icon="coupon" is-link value="内容"></van-cell>
-        </van-cell-group>
+            <van-cell-group style="margin-top:10px">
+                <van-cell title="我的积分" icon="exchange" is-link></van-cell>
+                <van-cell title="我的优惠券" is-link value="">
+                    <template slot="title">
+                        <i class="iconfont icon-coupon my-icon"></i>
+                        <span class="van-cell-text">我的优惠券</span>
+                    </template>
+                </van-cell>
+                <van-cell title="收货地址薄" icon="location" is-link></van-cell>
+            </van-cell-group>
+
+            <van-cell-group style="margin-top:10px">
+                <van-cell title="留言反馈" icon="chat" is-link></van-cell>
+            </van-cell-group>
+        </div>
+        
+        <van-tabbar v-model="active">
+            <van-tabbar-item icon="shop">杨家酱小火锅</van-tabbar-item>
+            <van-tabbar-item icon="contact">个人中心</van-tabbar-item>
+        </van-tabbar>
     </div>
 </template>
 
@@ -54,6 +79,7 @@ export default {
         return {
             msg: 'Welcome',
             images:['../assets/y1.jpg'],
+            active:1,
         }
     }
 }
@@ -64,15 +90,21 @@ export default {
         .head-img{
             height: 240px;
             width: 100%;
+            background-color: #ffffff;
             img{
                 height: 240px;
                 width: 100%;
             }
         }
+        .my-icon{
+            vertical-align: middle;
+            height: 19px;
+            line-height: 19px;
+        }
         .van-col{
             background-color: white;
             text-align: center;
-            padding: 10px 0;
+            padding: 20px 0 10px 0;
             i{
                 font-size: 24px;
             }
@@ -81,6 +113,12 @@ export default {
                 padding: 0;
                 font-size: 13px;
             }
+        }
+        .van-tabbar-item--active {
+            //color: red;
+        }
+        .my-links{
+            margin-bottom: 50px;
         }
     }
 </style>
