a8c9ba18e937124633d0f2676b7bb753d8aab89f
qhx0807 <820240134@qq.com>
2017-10-30 17:28:40 +0800

    update icons

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,22 @@
 
 <script>
 export default {
-	name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style>
-@import url('http://at.alicdn.com/t/font_456469_jnycso0dbh2gldi.css');
-html,body{
-	height: 100%;
-	width: 100%;
+@import url("//at.alicdn.com/t/font_456469_jnycso0dbh2gldi.css");
+html,
+body {
+  height: 100%;
+  width: 100%;
 }
 #app {
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	height: 100%;
-	width: 100%;
-	background-color: #f8f8f8;	
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  width: 100%;
+  background-color: #f8f8f8;
 }
 </style>
