5cddcf86f30144a7398e3a640a09d64545a8534f
qhx0807 <820240134@qq.com>
2017-11-03 02:59:27 +0800

    update vuex

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -4,17 +4,34 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-    listData: [],
+	listData: [],
+	detailObj:{},
 }
 
 export default new Vuex.Store({
-  	state,
- 	mutations: {
-	    UPDATE_LIST (state, data) {
-	      	state.listData = data
-	    }
-  	},
-  	actions: {
-	    
+	state,
+	mutations: {
+		UPDATE_LIST(state, data) {
+			state.listData = data
+		},
+
+		ADD_LIST_ITEM(state, id) {
+			let target = state.listData.find(function (item) {
+				return item.CommodityID == id
+			})
+			target.num += 1
+		},
+
+		SUB_LIST_ITEM(state, id) {
+			let target = state.listData.find(function (item) {
+				return item.CommodityID == id
+			})
+			if (target.num > 0) target.num -= 1
+			return false
+		},
+
+		UPDATE_dETAIL(state, obj){
+			state.detailObj = obj
+		}
 	}
 })
