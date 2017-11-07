d0a7ac9d933848ddaf0aa29c31e8b4ee45edf004
qhx0807 <820240134@qq.com>
2017-11-07 17:34:52 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,13 @@ const Detail = resolve => require(['@/components/buy/Detail'], resolve)
 const Group = resolve => require(['@/components/buy/Group'], resolve)
 const OrderInfo = resolve => require(['@/components/order/OrderInfo'], resolve)
 const PopuCenter = resolve => require(['@/components/popularize/PopuCenter'], resolve)
+const PopuUser = resolve => require(['@/components/popularize/PopuUser'], resolve)
+const MyOrder = resolve => require(['@/components/popularize/MyOrder'], resolve)
+const MySubUser = resolve => require(['@/components/popularize/MySubUser'], resolve)
+const CashRecords = resolve => require(['@/components/popularize/CashRecords'], resolve)
+const CashApply = resolve => require(['@/components/popularize/CashApply'], resolve)
+const SharePopu = resolve => require(['@/components/popularize/SharePopu'], resolve)
+const MySubUserOrder = resolve => require(['@/components/popularize/MySubUserOrder'], resolve)
 
 Vue.use(Router)
 
@@ -94,6 +101,46 @@ export default new Router({
 							component: Detail
 						}
 					]
+				},
+				{
+					path:'/auth/popuuser',
+					name:'popuuser',
+					component: PopuUser,
+				},
+				{
+					path:'/auth/myorder',
+					name:'myorder',
+					component: MyOrder,
+				},
+				{
+					path:'/auth/popumyorder',
+					name:'popumyorder',
+					component: MyOrder,
+				},
+				{
+					path:'/auth/mysubuser',
+					name:'mysubuser',
+					component: MySubUser,
+				},
+				{
+					path:'/auth/cashrecords',
+					name:'cashrecords',
+					component: CashRecords,
+				},
+				{
+					path:'/auth/cashapply',
+					name:'cashapply',
+					component: CashApply,
+				},
+				{
+					path:'/auth/mysubuserorder',
+					name:'mysubuserorder',
+					component: MySubUserOrder,
+				},
+				{
+					path:'/auth/popushare',
+					name:'popushare',
+					component: SharePopu,
 				},
 			]
 		}
