afa09ce02ae179378bd2c27317c214c1e4467289
qhx0807 <820240134@qq.com>
2017-12-23 23:48:50 +0800

    add act...

---
+++ src/components/activity/activity.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="act">
+    <img class="hedimg" src="../../assets/act1.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+.act{
+    .hedimg{
+        width: 100%;
+        background-color: #fff;
+    }
+}
+</style>
