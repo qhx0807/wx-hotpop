182f7be5e477b9c1eafac20ee027b7ce5f9cdb49
qhx0807 <820240134@qq.com>
2018-01-05 15:29:15 +0800

    share title

--- src/components/activity/activity.vue
+++ src/components/activity/activity.vue
@@ -16,7 +16,7 @@ export default {
         }
     },
     created () {
-        
+
     },
      activated(){
         this.fatherid = localStorage.openid
@@ -29,11 +29,11 @@ export default {
 			let timestamp = new Date().getTime().toString()
 			let url = window.location.href.split('#')[0]
 			let noncestr = "Wm3WZYTPz0wzccnW"
-			
+
 			let str = "jsapi_ticket="+ticket+"&noncestr="+noncestr+"&timestamp="+timestamp+"&url="+url;
 			let signature = sha1(str)
 			wx.config({
-				debug: false, 
+				debug: false,
 				appId: 'wx3a1714f5b4c11978',
 				timestamp: timestamp,
 				nonceStr: noncestr,
@@ -43,11 +43,11 @@ export default {
 
 			wx.ready(function(){
 				wx.onMenuShareTimeline({
-					title: "圣诞元旦，惊喜不断！",
-					desc:"随时随地，和你品尝舌尖上的重庆！",
+					title: "钱的事你不用担心",
+					desc:"关注公众号，免费吃火锅",
 					link: 'http://huoguo.cqjft.com/share/new-share.html?r=groupid_4DE30F20-A4A8-46A3-85BC-7AE28FCD29CF',
-					imgUrl: 'http://huoguo.cqjft.com/images/shareimg.bmp', 
-					success: function () { 
+					imgUrl: 'http://huoguo.cqjft.com/images/shareimg.bmp',
+					success: function () {
 						alert("分享成功！");
 					},
 					cancel: function () {
@@ -55,11 +55,11 @@ export default {
 					}
 				})
 				wx.onMenuShareAppMessage({
-					title: "圣诞元旦，惊喜不断！",
-					desc:"随时随地，和你品尝舌尖上的重庆！",
+					title: "钱的事你不用担心",
+					desc:"关注公众号，免费吃火锅",
 					link: 'http://huoguo.cqjft.com/share/new-share.html?r=groupid_4DE30F20-A4A8-46A3-85BC-7AE28FCD29CF',
-					imgUrl: 'http://huoguo.cqjft.com/images/shareimg.bmp', 
-					success: function () { 
+					imgUrl: 'http://huoguo.cqjft.com/images/shareimg.bmp',
+					success: function () {
 						alert("分享成功！");
 					},
 					cancel: function () {
